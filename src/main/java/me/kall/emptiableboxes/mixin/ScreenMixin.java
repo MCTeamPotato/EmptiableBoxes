f0f47840e714b93a6f59b5b54d5c528e08d6fed5
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-01-03 13:32:13 +0800

    1192

--- src/main/java/me/kall/emptiableboxes/mixin/ScreenMixin.java
+++ src/main/java/me/kall/emptiableboxes/mixin/ScreenMixin.java
@@ -1,9 +1,9 @@
 package me.kall.emptiableboxes.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.components.Widget;
 import net.minecraft.client.gui.screens.Screen;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
@@ -17,17 +17,17 @@ import java.util.List;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
-    @Shadow @Final public List<Renderable> renderables;
+    @Shadow @Final public List<Widget> renderables;
 
     @Inject(method = "render", at = @At("HEAD"))
-    private void clear(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+    private void clear(PoseStack poseStack, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
         long window = Minecraft.getInstance().getWindow().getWindow();
         boolean stateLeftShift = GLFW.glfwGetKey(window, GLFW.GLFW_KEY_LEFT_SHIFT) == GLFW.GLFW_PRESS;
         boolean stateRightShift = GLFW.glfwGetKey(window, GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS;
         boolean stateRightMouse = GLFW.glfwGetMouseButton(window, GLFW.GLFW_MOUSE_BUTTON_RIGHT) == GLFW.GLFW_PRESS;
 
         if (stateRightMouse && (stateRightShift || stateLeftShift)) {
-            for (Renderable renderable : this.renderables) {
+            for (Widget renderable : this.renderables) {
                 if (renderable instanceof EditBox) {
                     ((EditBox)renderable).setValue("");
                 }
