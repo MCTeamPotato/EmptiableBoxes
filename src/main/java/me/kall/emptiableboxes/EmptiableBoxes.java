2fd30ec6c9556790dab6dfab507c75286abc20d6
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-01-03 13:40:13 +0800

    1211

--- src/main/java/me/kall/emptiableboxes/EmptiableBoxes.java
+++ src/main/java/me/kall/emptiableboxes/EmptiableBoxes.java
@@ -1,6 +1,6 @@
 package me.kall.emptiableboxes;
 
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod(EmptiableBoxes.MOD_ID)
 public final class EmptiableBoxes {
